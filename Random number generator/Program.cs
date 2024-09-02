eff0d382048bd71ae1624bc3790fab23c20d4fae
Qwizdy <Hudsonyc@outlook.com>
2024-09-02 18:33:57 +0200

    Fixed the ending and hid the answer

--- Random number generator/Program.cs
+++ Random number generator/Program.cs
@@ -9,22 +9,25 @@ namespace Random_number_generator
         {
             Random r = new Random();
             var num = r.Next(1, 11);
-            Console.WriteLine(num);
             Console.WriteLine("Guess a number between one and ten");
-            var Usernum=Console.ReadLine();
-           var parsesuc=int.TryParse (Usernum, out int usernumber);
+            var Usernum = Console.ReadLine();
+            var parsesuc = int.TryParse(Usernum, out int usernumber);
             if (parsesuc == false)
-            { 
+            {
 
-            Console.WriteLine("you didn't input a number, please try again"); 
+                Console.WriteLine("you didn't input a number, please try again");
 
             }
             if (usernumber == num)
-            Console.WriteLine ("You got it :3") ;
-
-            if (parsesuc == Usernum)
+            {
+                Console.WriteLine("You got it :3");
+            }
+            else
+            {
                 Console.WriteLine("you got it wrong");
-           
+            }
+
+
 
 
         }
